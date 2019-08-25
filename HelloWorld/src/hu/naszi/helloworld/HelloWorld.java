90eca8d78081f674331ae316ee23f08aeae41282
Szabolcs Nagy <naszi75@gmail.com>
2019-08-25 07:16:40 +0200

    This is a third commit - simple calculator

--- HelloWorld/src/hu/naszi/helloworld/HelloWorld.java
+++ HelloWorld/src/hu/naszi/helloworld/HelloWorld.java
@@ -3,7 +3,19 @@ package hu.naszi.helloworld;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
+		
+		int a = 7;
+		int b = 5;
+		int c = a + b;
+		int d = a - b;
+		int e = a * b;
+		double f = (double) a / b;
+		
 		System.out.println("Hello World");
+		System.out.println("A ket szam osszege: " + c);
+		System.out.println("A ket szam kulonbsege: " + d);
+		System.out.println("A ket szam szorata: " + e);
+		System.out.println("A ket szam hanyadosa: " + f);
 	}
 
 }
